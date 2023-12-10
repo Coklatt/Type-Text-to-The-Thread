d0af5f489702bd4aa6bffd9a9698a6a690d1eef7
reader00 <qoryamanahputra0708@gmail.com>
2023-12-11 02:19:14 +0700

    push original

--- src/Domains/threads/comment/CommentRepository.js
+++ src/Domains/threads/comment/CommentRepository.js
@@ -1,19 +1,19 @@
 /* eslint-disable no-unused-vars */
 class CommentRepository {
     async verifyCommentExist(data) {
-        throw new Error('COMMENT_REPOSITORY.VERIFY_COMMENT_EXIST.METHOD_NOT_IMPLEMENTED');
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     }
 
     async addComment(addComment) {
-        throw new Error('COMMENT_REPOSITORY.ADD_COMMENT.METHOD_NOT_IMPLEMENTED');
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     }
 
     async getCommentsByThreadId(threadId) {
-        throw new Error('COMMENT_REPOSITORY.GET_COMMENTS_BY_THREAD_ID.METHOD_NOT_IMPLEMENTED');
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     }
 
     async deleteCommentById(commentId) {
-        throw new Error('COMMENT_REPOSITORY.DElETE_COMMENT.METHOD_NOT_IMPLEMENTED');
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     }
 }
 
